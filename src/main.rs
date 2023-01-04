5adafe574657d08a59ace44b84cb4bfb8b487840
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2023-01-04 03:04:43 -0600

    Complete refactor to lib.rs, abandon idea of build.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 
-use unishorten::{MAP, shorten_by_bytes, shorten_by_chars};
+use unishorten::{shorten_by_bytes, shorten_by_chars, load_map, store_map};
 use std::io::{self, Write};
 
 fn main() {
-    // print map
-    println!("Map:");
-    for (key, value) in MAP.iter() {
-        println!("{} -> {}", key, value);
-    }
+    let map = load_map("src/map.tsv").unwrap();
+    store_map(&map, "src/map.bincode").unwrap();
 
     let mut input = String::new();
     print!("Enter string to shorten: ");
@@ -24,7 +21,7 @@ fn main() {
     );
 
     // replace all
-    let replaced_bytes = shorten_by_bytes(&input);
+    let replaced_bytes = shorten_by_bytes(&input, &map);
     println!(
         "{: <30} {: <20} ({})",
         "Shortest in bytes:",
@@ -32,7 +29,7 @@ fn main() {
         replaced_bytes.len()
     );
 
-    let replaced_chars = shorten_by_chars(&input);
+    let replaced_chars = shorten_by_chars(&input, &map);
     println!(
         "{: <30} {: <20} ({})",
         "Shortest in characters used:",
